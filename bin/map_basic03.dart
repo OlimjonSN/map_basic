90b0d9b8b50099150d7f775a6997934f51d6305c
olimjonsn230 <olimjonsn230@gmail.com>
2022-07-22 12:04:33 +0500

    done

---
+++ bin/map_basic03.dart
@@ -0,0 +1,15 @@
+/*
+  
+  Given argument data type of Map check it length greater than zero 
+Return true if the length of the map is greater than zero, otherwise false.
+
+
+*/
+
+bool func(Map dct) {
+  return;
+}
+
+void main() {
+  print(func());
+}
